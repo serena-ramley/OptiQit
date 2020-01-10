24d815b89aca404a6bcc9e67cd2321ec2e038e31
melmatt7 <melmatt007@gmail.com>
2020-01-09 17:07:37 -0800

    rebase resolve

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-import React, {Component} from 'react';
-import './App.css';
-import BlankComponent from './BlankComponent';
-import ExperimentList from './ExperimentList';
-import { experiments } from './experiments';
-import { render } from '@testing-library/react';
+import React, { Component } from "react";
+import "./App.css";
+import BlankComponent from "./BlankComponent";
+import ExperimentList from "./ExperimentList";
+import { experiments } from "./experiments";
+import { render } from "@testing-library/react";
+import { NavigationBar } from "./components/NavigationBar";
+import { Instructions } from "./components/Instructions";
+import { Control } from "./components/Control";
+import { Results } from "./components/Results";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      experimentSelection: '',
-      experimentStatus: '',
-    }
+      experimentSelection: "",
+      experimentStatus: ""
+    };
   }
 
-    render() {
-      return (
-        <div className="tc">
-            <h1> OptiQit </h1>
-            <p> Add content</p>
-            <BlankComponent />
-            <ExperimentList experiments = {experiments} />
-        </div>
-    )
-    }
+  render() {
+    return (
+      <div className="tc">
+        <h1> OptiQit </h1>
+        <p> Add content</p>
+        <BlankComponent />
+        <ExperimentList experiments={experiments} />
+      </div>
+    );
   }
+}
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       experimentSelection: "",
+//       experimentStatus: ""
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <NavigationBar />
+//         <p> Add content</p>
+//         <BlankComponent />
+//         <Instructions />
+//         <Control />
+//         <Results />
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
