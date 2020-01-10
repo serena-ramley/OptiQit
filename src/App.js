07cf3effba5eee2f047074cf8ff7b79aa0c3d26f
melmatt7 <melmatt007@gmail.com>
2020-01-09 16:56:27 -0800

    task complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, {Component} from 'react';
-import './App.css';
-import BlankComponent from './BlankComponent';
-import { render } from '@testing-library/react';
+import React, { Component } from "react";
+import "./App.css";
+import BlankComponent from "./BlankComponent";
+import { NavigationBar } from "./components/NavigationBar";
+import { Instructions } from "./components/Instructions";
+import { Control } from "./components/Control";
+import { Results } from "./components/Results";
+
+import { render } from "@testing-library/react";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      experimentSelection: '',
-      experimentStatus: '',
-    }
+      experimentSelection: "",
+      experimentStatus: ""
+    };
   }
 
-    render() {
-      return (
-        <div>
-            <h1> OptiQit </h1>
-            <p> Add content</p>
-            <BlankComponent />
-        </div>
-    )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <p> Add content</p>
+        <BlankComponent />
+        <Instructions />
+        <Control />
+        <Results />
+      </React.Fragment>
+    );
   }
+}
 
 export default App;
