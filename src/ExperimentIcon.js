56a60785a0e60b0651dd1a236327f5cb2f4808f8
serena-ramley <sramley@gmail.com>
2020-01-10 11:21:36 -0800

    Experiment icons now all appear on same row, if screen size permits

--- src/ExperimentIcon.js
+++ src/ExperimentIcon.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ExperimentIcon = ( {id, name, imgSrc}) => {
     return (
-        // Use tachyons to create green background and grow upon hover
-        <div>
+        // Use tachyons to create circle br-pill
+        <div className='tc dib br3 pa3 ma2 grow bw2'>
             <h2>{name}</h2>
             <img className='br-pill' alt={name} src={`${imgSrc}`} width='200' height='200' />
         </div>
