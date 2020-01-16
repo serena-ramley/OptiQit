e76d492ce3e1b583ec5851905f7ba0308dacddb1
serena-ramley <sramley@gmail.com>
2020-01-16 14:06:29 -0800

    Differentiated experiments

--- src/ExperimentList.js
+++ src/ExperimentList.js
@@ -2,37 +2,41 @@ import React from "react";
 import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import ExperimentIcon from "./ExperimentIcon";
 import { Nav, Col, Row, Image } from "react-bootstrap";
+import ExperimentA from "./beamsplitter.png";
+import ExperimentB from "./twoslit.jpg";
+import ExperimentC from "./twoslitphotons.png";
 
 class ExperimentList extends React.Component {
   render() {
     return (
       <MDBContainer className="d-flex justify-content-around">
         <MDBRow>
-          <MDBCol md="4">
-            <Nav>
-              <Nav.Link href="/experiment">
-                <MDBView hover>
-                  <img
-                    src="https://cdn.arstechnica.net/wp-content/uploads/2016/10/slide-1-800x401.jpg"
-                    className="img-fluid rounded-circle hoverable"
-                    alt=""
-                  />
-                  <MDBMask
-                    className="flex-center rounded-circle"
-                    overlay="red-strong"
-                  >
-                    <p className="white-text">Experiment A</p>
-                  </MDBMask>
-                </MDBView>
-              </Nav.Link>
-            </Nav>
+        <MDBCol>
+            <MDBView hover>
+            <div style={{width: 200, height: 'auto'}}>
+              <img
+                src={ExperimentA}
+                className="img-fluid rounded-circle hoverable"
+                alt=""
+                responsive
+              />
+              <MDBMask
+                className="rounded-circle flex-center"
+                overlay="red-strong"
+              >
+                <p className="white-text">Experiment A</p>
+              </MDBMask>
+              </div>
+            </MDBView>
           </MDBCol>
           <MDBCol md="4">
             <MDBView hover>
+            <div style={{width: 200, height: 'auto'}}>
               <img
-                src="https://cdn.arstechnica.net/wp-content/uploads/2016/10/slide-1-800x401.jpg"
+                src={ExperimentB}
                 className="img-fluid rounded-circle hoverable"
                 alt=""
+                responsive
               />
               <MDBMask
                 className="flex-center rounded-circle"
@@ -40,14 +44,17 @@ class ExperimentList extends React.Component {
               >
                 <p className="white-text">Experiment B</p>
               </MDBMask>
+              </div>
             </MDBView>
           </MDBCol>
           <MDBCol>
             <MDBView hover>
+            <div style={{width: 200, height: 'auto'}}>
               <img
-                src="https://cdn.arstechnica.net/wp-content/uploads/2016/10/slide-1-800x401.jpg"
+                src={ExperimentC}
                 className="img-fluid rounded-circle hoverable"
                 alt=""
+                responsive
               />
               <MDBMask
                 className="rounded-circle flex-center"
@@ -55,6 +62,7 @@ class ExperimentList extends React.Component {
               >
                 <p className="white-text">Experiment C</p>
               </MDBMask>
+              </div>
             </MDBView>
           </MDBCol>
         </MDBRow>
