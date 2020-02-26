8e6ad5626306fd9249324d5763f3885a88d0bd7a
melmatt7 <melmatt007@gmail.com>
2020-02-25 23:48:29 -0800

    work

---
+++ api/routes/data.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+
+const data = [
+  { position: 1, count: 0 },
+  { position: 2, count: 0 },
+  { position: 3, count: 0 },
+  { position: 4, count: 0 },
+  { position: 5, count: 0 },
+  { position: 6, count: 0 },
+  { position: 7, count: 0 },
+  { position: 8, count: 0 },
+  { position: 9, count: 0 },
+  { position: 10, count: 0 }
+];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function increment() {
+  const num = getRandomNumber(0, data.length);
+  data[num].votes += 20;
+}
+
+function updateCount() {
+  setInterval(() => {
+    increment();
+    pusher.trigger("poll-channel", "update-poll", {
+      poll
+    });
+  }, 1000);
+}
+
+router.get("/data", function(req, res, next) {
+  res.json(data);
+  updateCount();
+});
+
+module.exports = router;
