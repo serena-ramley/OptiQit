e5ee57e66c598e7e2085503f4223ed50fa9db4c3
serena-ramley <sramley@gmail.com>
2020-03-21 21:22:32 -0700

    Created two versions of navigation bar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 
-export const NavigationBar = () => (
+export const NavigationBarWithControl = () => (
   <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
     <Navbar.Toggle aria-controls="basic-navbar" />
 
@@ -15,6 +15,25 @@ export const NavigationBar = () => (
 
       <Nav.Item>
         <Nav.Link href="#control">Control</Nav.Link>
+      </Nav.Item>
+
+      <Nav.Item>
+        <Nav.Link href="#results">Results</Nav.Link>
+      </Nav.Item>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export const NavigationBarWithoutControl = () => (
+  <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
+    <Navbar.Toggle aria-controls="basic-navbar" />
+
+    <Navbar.Collapse
+      id="basic-navbar-nav"
+      className="d-flex justify-content-around"
+    >
+      <Nav.Item>
+        <Nav.Link href="#instructions">Instructions</Nav.Link>
       </Nav.Item>
 
       <Nav.Item>
