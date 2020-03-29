569717d2e14dd771a326d5aaf92b0e77abc6538c
melmatt7 <mel.matt007@gmail.com>
2020-03-29 01:49:24 -0700

    hypothesis updates, instruction updates

---
+++ client/src/components/Introduction.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Introduction = () => (
+  <div id="introduction">
+    <h1 >Introduction</h1>
+    <p>
+      {" "}
+      Enter introductions here
+    </p>
+  </div>
+);
+
+
+export const IntroductionDoubleSlit = () => (
+  <div id="introduction">
+    <h2 >Introduction</h2>
+    <p>
+      {" "}
+      When a beam of light reaches a "beam splitter", the light can either be transmitted (go through) or be reflected (bounce back).
+      If the beam of light is aligned at 45 degrees with respect to the beam splitter,
+      then it will be equally likely for each particle of light to be transmitted or reflected.
+    </p>
+  </div>
+);
+
+
+
+export const IntroductionsLowPhotonDoubleSlit = () => (
+  <div id="introductions">
+    <h2 >Introduction</h2>
+    <p>
+      {" "}      
+      What is a photon? Light that you see from a light bulb or from the sun is believed
+      to be made up of many "photons" or particles of light.
+      Light behaves both as a particle and as a wave. When you place two slits in front of a beam of light (made up of many photons),
+      what do you think determines which way each particle will go? What happens when the photons bump into each other?
+      Here we will try the "double slit experiment" with a small number of photons - there is light, but there is so little light that your eyes can't detect it!
+      However, the electronic single photon detector will absorb photons and convert light energy to electrical energy.
+      OptiQit will report how many photons are detected in a short time interval at each position.
+    </p>
+  </div>
+);
